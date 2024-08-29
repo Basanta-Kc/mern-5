f63ef674a78b03a605333fc7468074ab2cc5af1a
Basanta-Kc <devbasanta@gmail.com>
2024-08-29 16:10:50 +0545

    data types

---
+++ javascript/day2.js
@@ -0,0 +1,27 @@
+// x = a + b; (%, **)
+
+// const name = "basanta";
+// const greet = "hello " + name;
+// console.log(greet);
+
+// // const result = 2 + 3;
+// // console.log(result);
+
+// // + (concatenation, addition)
+
+// const test = 2 + 2 + "2" + 3; // 4 + "2" => "42" + 3 => 423
+// const sub = null - 1;
+
+// console.log(sub);
+// // null => 0
+// // "1" => 1
+// // "  1   " => 1
+// // undefined = Nan
+// // true => 1
+// // false = 0
+
+// let count = 0;
+// const result = ++count; // count = count + 1
+// console.log(result, count); // count++ => 1, ++count => 0,1
+console.log("0" === false);
+// null => 0, undeinfe => Nan
